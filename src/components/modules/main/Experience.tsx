0f4fc6fd09f107456ee3f167bbe5c5cc56588bbc
philipnwabuwa <philipnwabuwa@gmail.com>
2024-06-27 14:35:25 +0000

    update: sales mode

--- src/components/modules/main/Experience.tsx
+++ src/components/modules/main/Experience.tsx
@@ -10,10 +10,7 @@ import Giftcard2 from "@/assets/images/main/gift-card2.png";
 import Giftcard3 from "@/assets/images/main/gift-card3.png";
 import freelancers1 from "@/assets/images/main/freelancers1.png";
 import freelancers2 from "@/assets/images/main/freelancers2.png";
-import sales1 from "@/assets/images/main/sales1.png";
-import sales2 from "@/assets/images/main/sales2.png";
-import sales3 from "@/assets/images/main/sales3.png";
-import sales4 from "@/assets/images/main/sales4.png";
+import sales from "@/assets/images/main/SalesMode-main.svg";
 import map from "@/assets/images/main/map.png";
 
 const Experience = () => {
@@ -108,7 +105,7 @@ const Experience = () => {
           </div>
           <Image src={map} alt="" className="mt-[64px] h-[413px]" />
         </div>
-        <div className="bg-[#E9F4FA] w-full rounded-[32px] p-[28px] h-[1045px] relative overflow-hidden">
+        <div className="bg-[#E9F4FA] w-full rounded-[32px] p-[28px] h-[1045px]">
           <div className="p-[24px]">
             <Image src={Icon3} alt="" />
             <h3 className="text-[68px] font-bold text-[#272A2B] leading-[80px] my-[24px]">
@@ -122,16 +119,9 @@ const Experience = () => {
               Learn More
             </button>
           </div>
-          <div className="w-full max-w-[990px]">
-            <div className="relative flex justify-between">
-              <Image className="absolute left-0 top-0" src={sales1} alt="" />
-              <Image className="absolute left-1/4 top-0" src={sales2} alt="" />
-              <Image className="absolute left-1/2 top-0" src={sales3} alt="" />
-              <Image
-                className="absolute left-[77%] top-0 rounded-[32px]"
-                src={sales4}
-                alt=""
-              />
+          <div className="w-full">
+            <div className="">
+              <Image className="" src={sales} alt="" />
             </div>
           </div>
         </div>
